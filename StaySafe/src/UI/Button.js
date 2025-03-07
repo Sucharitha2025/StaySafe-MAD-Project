04f64cecebf3e8790f4351d5b18fc067a3f43610
SidraBaig08 <k2206263@kingston.ac.uk>
2025-03-07 17:49:52 +0000

    Icon and Button

---
+++ StaySafe/src/UI/Button.js
@@ -0,0 +1,32 @@
+import { Pressable, StyleSheet, Text } from 'react-native';
+
+const Button = ({label, onClick }) => {
+    //initialiastion 
+    //state
+    //handlers
+    //view
+    return (
+        <Pressable onPress={onClick} style={StyleSheet.button}>
+            <Text style={styles.label}>{label}</Text>
+        </Pressable>
+    );
+};
+
+const styles = StyleSheet.create({
+    button: {
+        minHeight: 50,
+        borderWidth: 1,
+        borderRadius: 7,
+        borderColor: 'grey',
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 3,
+        flex: 1,
+    },
+    label: {
+        fontSize: 16,
+    },
+});
+
+export default Button;
