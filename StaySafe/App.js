cbfa8c61ea8277af8d748b9afcbfb85d190b0270
Sucharitha Gangapatnam <sucharitha.22@outlook.com>
2025-03-20 21:14:10 +0000

    Implemented MapScreen

--- StaySafe/App.js
+++ StaySafe/App.js
@@ -1,47 +1,18 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { useAuth ,AuthContextProvider,AuthContext} from "./src/Context/authContext";
-import { navigationRef, isNavigationReady } from "./src/Navigation/navigationRef";
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import MapScreen from './src/Screens/MapScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import SignIn from "./src/Screens/SignIn";
-import SignUp from "./src/Screens/SignUp";
-import MapScreen from "./src/Screens/MapScreen"; // Ensure this is imported
-import  { useRef,useEffect } from "react";
 const Stack = createNativeStackNavigator();
 
-
-
-
 const App = () => {
-  const { isAuthenticated } = useAuth(); 
-
-  useEffect(() => {
-    if (navigationRef.current) {
-      isNavigationReady(true);
-    }
-  }, []);
-
   return (
-    <NavigationContainer ref={navigationRef}>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {isAuthenticated ? (
-          <Stack.Screen name="MapScreen" component={MapScreen} />
-        ) : (
-          <>
-            <Stack.Screen name="SignIn" component={SignIn} />
-            <Stack.Screen name="SignUp" component={SignUp} />
-          </>
-        )}
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Maps" component={MapScreen} />
       </Stack.Navigator>
     </NavigationContainer>
-  );
-};
-
-export default function Root() {
-  return (
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
-  );
+  )
 }
+
+export default App;
