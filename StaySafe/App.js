1c15776cdbffef4a1b174f493953b2a560e51b64
SidraBaig08 <k2206263@kingston.ac.uk>
2025-03-25 00:00:41 +0000

    new panic button

--- StaySafe/App.js
+++ StaySafe/App.js
@@ -1,14 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert } from 'react-native';
 import Button from './src/UI/Button'
 
 export const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Help</Text>
+      <Text style={styles.text}>*Warning this button will send an alert to emergency contacts</Text>
       <StatusBar style="auto" />
-      <Button label='Panic button' onClick={() => alert.Alert('Warning Button Pressed')}
-        color="red"
+      <Button label='SOS' onPress={() => Alert.alert('Warning Button Pressed: Emergency notification sent')}
+        color= 'red'
         />
     </View>
   );
@@ -17,10 +17,17 @@ export const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    gap: 20,
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    marginTop: 20,
+    color: 'white',
+    fontSize: 20,
+    textAlign: 'center',
+  },
 });
 
-export default App;
+export default App; 
--- StaySafe/src/UI/Button.js
+++ StaySafe/src/UI/Button.js
@@ -1,12 +1,12 @@
 import { Pressable, StyleSheet, Text } from 'react-native';
 
-const Button = ({label, onClick }) => {
+const Button = ({label, onPress, color }) => {
     //initialiastion 
     //state
     //handlers
     //view
     return (
-        <Pressable onPress={onClick} style={StyleSheet.button}>
+        <Pressable onPress={onPress} style={[styles.button, color && { backgroundColor: color }]}>
             <Text style={styles.label}>{label}</Text>
         </Pressable>
     );
@@ -14,18 +14,23 @@ const Button = ({label, onClick }) => {
 
 const styles = StyleSheet.create({
     button: {
-        minHeight: 50,
+        minHeight: 200,
+        minWidth: 200,
         borderWidth: 1,
-        borderRadius: 7,
-        borderColor: 'grey',
+        borderRadius: 100,
+        borderColor: 'white',
+        borderWidth: 15,
         backgroundColor: 'white',
         alignItems: 'center',
         justifyContent: 'center',
         padding: 3,
-        flex: 1,
+        
     },
     label: {
-        fontSize: 16,
+        fontSize: 32,
+        fontWeight: 'bold',
+        
+        
     },
 });
 
