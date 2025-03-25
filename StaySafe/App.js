47543b042dd4e971d291d89fb311e5f1f4dafef7
SidraBaig08 <k2206263@kingston.ac.uk>
2025-03-25 01:41:30 +0000

    panic button completed

--- StaySafe/App.js
+++ StaySafe/App.js
@@ -1,15 +1,35 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Alert } from 'react-native';
-import Button from './src/UI/Button'
+import Button from './src/UI/Button';
+import { Audio } from 'expo-av';  
 
 export const App = () => {
+  const [sound, setSound] = React.useState();
+
+  // Function to play the sound
+  const playSound = async () => {
+    const { sound } = await Audio.Sound.createAsync(
+      require('./assets/sos.mp3') 
+    );
+    setSound(sound);
+    await sound.playAsync();  
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>*Warning this button will send an alert to emergency contacts</Text>
+      <Text style={styles.text}>
+        *Warning this button will send an alert to emergency contacts
+      </Text>
       <StatusBar style="auto" />
-      <Button label='SOS' onPress={() => Alert.alert('Warning Button Pressed: Emergency notification sent')}
-        color= 'red'
-        />
+      <Button 
+        label="SOS" 
+        onPress={() => {
+          playSound();  
+          Alert.alert('Warning Button Pressed: Emergency notification sent');
+        }} 
+        color="red" 
+      />
     </View>
   );
 };
@@ -30,4 +50,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App; 
+export default App;
